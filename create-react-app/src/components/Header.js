475b0c981320f6901d8c1606e774ea35cbc882f0
Maya Morales <mayainomorales@gmail.com>
2022-06-01 06:20:31 -0700

    add functions for corresponding js files in components

---
+++ create-react-app/src/components/Header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+// Here we import the Header.css file to grant access to some additional classNames
+import '../styles/Header.css';
+
+
+function Header() {
+
+  return (
+    <header className="header">
+      <h1></h1>
+    </header>
+  );
+}
+
+export default Header;
---
+++ create-react-app/src/components/Navigation.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+import '../styles'
+
+function Navigation() {
+
+    return (
+      <header className="header">
+        <h1></h1>
+      </header>
+    );
+  }
+  
+  export default Navigation;
