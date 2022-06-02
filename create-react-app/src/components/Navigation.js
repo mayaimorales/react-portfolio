7114fae2a3efde73908e40bf8eb7843008a60a76
Maya Morales <mayainomorales@gmail.com>
2022-06-01 22:29:48 -0700

    add footer and navbar

--- create-react-app/src/components/Navigation.js
+++ create-react-app/src/components/Navigation.js
@@ -4,11 +4,21 @@ import '../styles'
 
 function Navigation() {
 
-    return (
-      <header className="header">
-        <h1></h1>
-      </header>
-    );
-  }
-  
-  export default Navigation;
+  return (
+    <div class="navbar bg-base-100">
+      <div class="flex-1">
+        <a class="btn btn-ghost normal-case text-xl">Maya Morales</a>
+      </div>
+      <div class="flex-none">
+        <ul class="menu menu-horizontal p-0">
+          <li><a>About Me</a></li>
+          <li><a>Portfolio</a></li>
+          <li><a>Contact Me</a></li>
+          <li><a>Resume</a></li>
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default Navigation;
